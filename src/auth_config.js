9cd321a9d29032447c56f71dc299e2b339683b64
faeezs <faeezs@gmail.com>
2022-03-28 22:54:35 +0700

    next

--- src/auth_config.js
+++ src/auth_config.js
@@ -2,8 +2,10 @@
 
   
 const config = {
-	domain: import.meta.env.VITE_PUBLIC_AUTH0_DOMAIN,
-	clientId: import.meta.env.VITE_PUBLIC_AUTH0_CLIENT_ID
+	// domain: import.meta.env.VITE_PUBLIC_AUTH0_DOMAIN,
+	// clientId: import.meta.env.VITE_PUBLIC_AUTH0_CLIENT_ID
+    domain: "dev-kceyl14s.us.auth0.com",
+	clientId: "kNTmWx5GhXsY2CDqo7UQrqC6SVP4vYeV"
 };
 
 export default config;
