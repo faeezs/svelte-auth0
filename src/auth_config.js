5e0e66c81100f2a8f4b568949e6debbbb5eb2d57
faeezs <faeezs@gmail.com>
2022-03-29 01:08:36 +0700

    updated enviroment variables

--- src/auth_config.js
+++ src/auth_config.js
@@ -1,11 +1,5 @@
-// auth_config.js
-
-  
 const config = {
-	// domain: import.meta.env.VITE_PUBLIC_AUTH0_DOMAIN,
-	// clientId: import.meta.env.VITE_PUBLIC_AUTH0_CLIENT_ID
-    domain: "dev-kceyl14s.us.auth0.com",
-	clientId: "kNTmWx5GhXsY2CDqo7UQrqC6SVP4vYeV"
+	domain: import.meta.env.VITE_PUBLIC_AUTH0_DOMAIN,
+	clientId: import.meta.env.VITE_PUBLIC_AUTH0_CLIENT_ID
 };
-
 export default config;
